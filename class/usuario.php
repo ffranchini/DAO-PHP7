65daab0ed54a30fe3720baf119a98201f6dd07f6
ffranchini <franchini_26@hotmail.com>
2024-06-19 23:03:03 -0300

    Criada a classe usuário

--- class/usuario.php
+++ class/usuario.php
@@ -39,7 +39,38 @@ class Usuario {
 		return $this->dtcadastro = $value;
 	}
 
+	public function loadbyId($id) {
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+			":ID"=>$id
+		));
+
+		if (count($results) > 0) {
+
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+		}
+		
+	}
+
+public function __toString(){
+
+	return json_encode(array(
+		"idusuario"=>$this->getIdusuario(),
+		"deslogin"=>$this->getDeslogin(),
+		"dessenha"=>$this->getDessenha(),
+		"dtcadastro"=>$this->getDtcadastro()->format("d/m/y H:i:s")
+	));
+
 }
 
+}
 
  ?> 
