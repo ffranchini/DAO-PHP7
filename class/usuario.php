937167097e33192078ba4bf53615897b2c031e81
ffranchini <franchini_26@hotmail.com>
2024-06-20 01:45:14 -0300

    Atualizacao

--- class/usuario.php
+++ class/usuario.php
@@ -57,7 +57,7 @@ class Usuario {
 			$this->setDtcadastro(new DateTime($row['dtcadastro']));
 
 		}
-		
+
 	}
 
 public function __toString(){
@@ -68,6 +68,50 @@ public function __toString(){
 		"dessenha"=>$this->getDessenha(),
 		"dtcadastro"=>$this->getDtcadastro()->format("d/m/y H:i:s")
 	));
+
+}
+
+public static function getList() {
+
+	$sql = new Sql();
+
+	return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+
+}
+
+public static function search($login) {
+
+	$sql = new Sql();
+
+	return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+		':SEARCH'=>"%".$login."%"
+));
+
+}
+
+public function login($login, $password){
+
+	$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(
+			":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		if (count($results) > 0) {
+
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+		} else {
+
+			throw new Exception("Login e/ou senha inválidos!");
+
+		}
 
 }
 
--- index.php
+++ index.php
@@ -2,10 +2,24 @@
 
 require_once("config.php");
 
-$root = new Usuario();
 
-$root->loadbyId(3);
+//carrega um usuario
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
 
-echo $root;
+//carega lista de usuario
+//$lista= Usuario::getList();
+//echo json_encode($lista);
+
+//carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
+
+
+//carrega usuario usando login e senha
+$usuario = new Usuario();
+$usuario->login("root", "54321");
+echo $usuario;
 
 ?>
